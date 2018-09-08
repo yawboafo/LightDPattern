da73f0846b8e2527776a802b4b48c9853d68028c
Engineer 144 <engineer144@macbooks-macbook-pro.local>
2018-09-08 21:49:50 +0000

    Initial Commit

---
+++ MobileMoneyPayment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MobileMoneyPayment
+//
+//  Created by Engineer 144 on 08/09/2018.
+//  Copyright © 2018 Engineer 144. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
